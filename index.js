36b4ff18b4b3bb39ea8fae9dcf71218b68fcff77
Wazed Biplob <www.biplob@gmail.com>
2023-05-02 04:27:52 +0600

    Chef ID API implemented

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const data = require("./data.json");
+const cors = require("cors");
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("The server is running.");
 });
 app.get("/data", (req, res) => {
   res.send(data);
 });
-
+app.get("/data/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chefId = data.find((chef) => parseInt(chef.id) === id);
+  res.send(chefId);
+});
 app.listen(port, () => {
   console.log(`running on ${port}`);
 });
